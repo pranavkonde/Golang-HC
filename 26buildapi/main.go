567abfed12f2106d5cc16aa52e5b6011e17c4276
Pranav Konde <pranavkonde2020@gmail.com>
2022-10-19 23:50:39 +0530

    Added buildapi

---
+++ 26buildapi/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// model for courses-file
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+// fake DB
+
+var courses []Course
+
+// middleware,helpers - file
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+
+}
+
+// controllers - file
+// serve home route
+
+func ServeHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API</h1>"))
+}
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+
+}
