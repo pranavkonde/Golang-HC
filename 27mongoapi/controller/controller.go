b136e05fd964a251d61fef16fb116a82e49c902c
Pranav Konde <pranavkonde2020@gmail.com>
2022-10-28 22:28:14 +0530

    updated mongoapi

--- 27mongoapi/controller/controller.go
+++ 27mongoapi/controller/controller.go
@@ -2,8 +2,12 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/driver/mongocrypt/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const connectionString = "mongodb+srv://pranavkonde:<Pradip@123>@cluster0.3609jz4.mongodb.net/?retryWrites=true&w=majority"
@@ -20,5 +24,65 @@ func init() {
 	//client option
 	clientOption := options.Client().ApplyURI(connectionString)
 	// connect to mongodb
-	mongo.connect(context.T)
+	client, err := mongo.connect(context.TODO(), clientOption)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MongoDB connection success")
+	//context stores request time, and other details
+
+	collection = client.Database(dbName).Collection(colName)
+
+	//collection instance
+	fmt.Println("collection reference is ready  ")
+}
+
+// MongoDB helpers -file
+
+// insert 1 record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted one movie in DB with id: ", inserted.InsertedID)
+}
+
+//update 1 record
+func updateOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.updateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified count:", result.ModifiedCount)
+}
+
+//delete one movie
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie got delete with deletecount", deleteCount)
+}
+
+//delete all reocrds from monfodb
+func deleteAllMovie() int64 {
+	deleteResult, err := collection.DeleteOne(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Number of movies deleted: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
+
+}
+func getAllMovies() {
+
 }
