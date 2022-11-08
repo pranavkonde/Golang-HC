13b1703c5b1e16c1fa4a662a6fd6a6ea998e0071
Pranav Konde <pranavkonde2020@gmail.com>
2022-11-08 23:30:24 +0530

    Added goroutines

--- 28goroutines/main.go
+++ 28goroutines/main.go
@@ -1,4 +1,26 @@
 // parallelism := doing multiple task at same time
 // concurrency := doing one task at one time
 
+// Thread - Managed by OS, fixedstack-1MB
+// Goroutines - Managed by Go runtime, Flexible stack-2KB
 
+//Goroutine - Do not communicate by sharing memory, instead share memory by communicating
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go greeter("Hello") //if sleep is not given than only world will be printed
+	greeter("World")
+
+}
+func greeter(s string) {
+	for i := 0; i < 6; i++ {
+		time.Sleep(5 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
