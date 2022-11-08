40d7c7f8df0d70ca5181216dc6735f0550511c23
Pranav Konde <pranavkonde2020@gmail.com>
2022-11-09 00:08:38 +0530

    Updated goroutines

--- 28goroutines/main.go
+++ 28goroutines/main.go
@@ -10,17 +10,38 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
-func main() {
-	go greeter("Hello") //if sleep is not given than only world will be printed
-	greeter("World")
+var wg sync.WaitGroup //pointer
 
+func main() {
+	// go greeter("Hello") //if sleep is not given than only world will be printed
+	// greeter("World")
+	websiteList := []string{
+		"https://lco/dev",
+		"https://github.com",
+		"https://fb.com",
+	}
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1) // 1=no. of goroutines
+	}
+	wg.Wait()
 }
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(5 * time.Millisecond)
-		fmt.Println(s)
+
+// func greeter(s string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(5 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+func getStatusCode(endpoint string) {
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	} else {
+		fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
 	}
 }
